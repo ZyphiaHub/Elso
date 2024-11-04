bf1d0f5e26b655792e08ffd0926936ebded75c35
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2024-11-04 22:23:39 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Frontend projects</h1>
+
+<div class="linklada">
+    <a href="https://zyphiahub.github.io/Receptkonyv/" class="button">Receptkönyv</a>
+    <a href="https://example.com" class="button">...</a>
+    <a href="https://example.com" class="button">...</a>
+</div>
+<p>
+        <a target="_blank" href="https://github.com/ZyphiaHub/Frontend.git">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/free-github-169-1174970.png" alt="GitHub"></a>
+</p>
+    
+<style>
+    h1 {
+        color: aliceblue;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        background-color: #435d7a;
+        text-align: center;
+        }
+    a {
+        font-size: 20;
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+    p {
+        text-align: center;
+    }
+    img {
+        
+        max-width: 50px;
+        
+    }
+    .button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #435d7a;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-size: 16px;
+  text-align: center;
+}
+.button:hover {
+  background-color: #93b7de;
+}
+.linklada {
+    align-content: space-around;
+    display: flex;
+    gap: 10px; /* Térköz a gombok között */
+    justify-content: center; /* Igazítás középre */
+    flex-wrap: wrap; /* Törés, ha nem férnek el egymás mellett */
+}
+
+    
+</style>
